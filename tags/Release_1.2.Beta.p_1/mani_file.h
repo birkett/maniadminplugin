62ce686e3f2a1979ff11e2b4f467c08ada7e0900
giles.millward <giles.millward@ce093b64-037c-11df-897e-95fb7fca2173>
2009-07-02 00:39:54 +0000

    Tag the 1.2 Beta p release (fixed p version)

---
+++ tags/Release_1.2.Beta.p_1/mani_file.h
@@ -0,0 +1,52 @@
+//
+// Mani Admin Plugin
+//
+// Copyright (c) 2009 Giles Millward (Mani). All rights reserved.
+//
+// This file is part of ManiAdminPlugin.
+//
+// Mani Admin Plugin is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Mani Admin Plugin is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Mani Admin Plugin.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+//
+
+
+
+#ifndef MANI_FILE_H
+#define MANI_FILE_H
+
+#if defined __WIN32__ || defined _WIN32 || defined WIN32
+	#define PATH_SEP_CHAR	'\\'
+	#define ALT_SEP_CHAR	'/'
+#elif defined __linux__
+	#define PATH_SEP_CHAR	'/'
+	#define ALT_SEP_CHAR	'\\'
+#endif
+
+class ManiFile
+{
+public:
+	ManiFile();
+	~ManiFile();
+
+	FILE		*Open(const char *filename, const char *attrib);
+	void		Close(FILE *fh);
+	int			Write(const void *pOutput, int size, FILE *fh);
+	char		*ReadLine(char *pOutput, int maxChars, FILE *fh);
+	void		PathFormat(char *buffer, size_t len, const char *fmt, ...);
+};
+
+extern	ManiFile *gpManiFile;
+
+#endif
---
+++ tags/Release_1.2.Beta.p_1/mani_messagemode.h
@@ -0,0 +1,67 @@
+//
+// Mani Admin Plugin
+//
+// Copyright (c) 2009 Giles Millward (Mani). All rights reserved.
+//
+// This file is part of ManiAdminPlugin.
+//
+// Mani Admin Plugin is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Mani Admin Plugin is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Mani Admin Plugin.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+//
+
+
+
+#ifndef MANI_MESSAGEMODE_H
+#define MANI_MESSAGEMODE_H
+
+#include "mani_player.h"
+#include "cbaseentity.h"
+
+class ManiMessageMode
+{
+public:
+	ManiMessageMode();
+	~ManiMessageMode();
+
+	void		NetworkIDValidated(player_t *player_ptr);
+	void		ClientDisconnect(player_t *player_ptr);
+	void		Load(void);
+	void		Unload(void);
+	void		LevelInit(void);
+	PLUGIN_RESULT	ProcessMaPMess(player_t *player_ptr, const char *command_name, const int help_id, const int command_type);
+	PLUGIN_RESULT	ProcessMaExit(player_t *player_ptr, const char *command_name, const int help_id, const int command_type);
+
+	PLUGIN_RESULT	ProcessMaPSay(player_t *player_ptr, const char *command_name, const int help_id, const int command_type);
+	PLUGIN_RESULT	ProcessMaMSay(player_t *player_ptr, const char *command_name, const int help_id, const int command_type);
+	PLUGIN_RESULT	ProcessMaSay(player_t *player_ptr, const char *command_name, const int help_id, const int command_type);
+	PLUGIN_RESULT	ProcessMaCSay(player_t *player_ptr, const char *command_name, const int help_id, const int command_type);
+	PLUGIN_RESULT	ProcessMaChat(player_t *player_ptr, const char *command_name, const int help_id, const int command_type);
+private:
+
+	void		CleanUp(void);
+	void		DisableMessageMode(int index);
+
+	struct	mess_mode_t
+	{
+		bool	admin_psay;
+		bool	admin_psay_list[MANI_MAX_PLAYERS];
+	};
+
+	mess_mode_t	mess_mode_list[MANI_MAX_PLAYERS];
+};
+
+extern	ManiMessageMode *gpManiMessageMode;
+
+#endif
