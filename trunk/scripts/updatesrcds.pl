3ebd184ee8f16138c4ef7aa7b2fcfea67bd27988
giles.millward <giles.millward@ce093b64-037c-11df-897e-95fb7fca2173>
2009-07-08 14:15:31 +0000

    Add Left4Dead server installation/update option

--- trunk/scripts/updatesrcds.pl
+++ trunk/scripts/updatesrcds.pl
@@ -30,7 +30,8 @@ $SRCDS_PATH="$ROOT_PATH/srcds_1";
 "Counter-Strike Source"		=> "Counter-Strike Source",
 "Half-Life 2 Deathmatch" 	=> "hl2mp",
 "Team Fortress 2" 		=> "tf",
-"Day of Defeat Source" 		=> "dods"
+"Day of Defeat Source" 		=> "dods",
+"Left 4 Dead" 			=> "l4d_full"
 );
 
 
