f77b630ccc4904f385d4ec2419230ac751e6db63
keeper.map <keeper.map@gmail.com>
2009-12-01 18:26:46 +0000

    Going Gold.

--- mani_admin_plugin/mani_main.h
+++ mani_admin_plugin/mani_main.h
@@ -48,7 +48,7 @@ const int	gametypes_min_version = 2;
 #define snprintf _snprintf
 #endif
 
-#define COMMON_CORE "1.2 BetaT RC2.13"
+#define COMMON_CORE "1.2T"
 #define COMMON_VERSION "Mani Admin Plugin 2009 V" COMMON_CORE
 
 #ifdef ORANGE
