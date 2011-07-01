8c3e15d181f6cb08886d25e36d358234904f85f2
keeper.map <keeper.map@gmail.com>
2011-07-01 18:16:05 +0000

    Updating revision just to recompile against altered SDK.

--- mani_admin_plugin/mani_main.h
+++ mani_admin_plugin/mani_main.h
@@ -49,7 +49,7 @@ const int	gametypes_min_version = 3;
 #endif
 
 //majorrelease.minorrealse.patchlevel.buildnumber
-#define COMMON_CORE "1.2.22.5"
+#define COMMON_CORE "1.2.22.6"
 #define COMMON_VERSION "Mani Admin Plugin 2011 V" COMMON_CORE
 
 #ifdef ORANGE
