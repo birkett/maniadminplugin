7395c54d72c7122e19eae86f6131f31a160dbd08
keeper.map <keeper.map@ce093b64-037c-11df-897e-95fb7fca2173>
2012-08-16 19:28:12 +0000

    Updated and recompiled with engine changes on 2012-08-16.

--- trunk/mani_admin_plugin/mani_main.h
+++ trunk/mani_admin_plugin/mani_main.h
@@ -49,7 +49,7 @@ const int	gametypes_min_version = 3;
 #endif
 
 //majorrelease.minorrealse.patchlevel.buildnumber
-#define COMMON_CORE "1.2.22.11"
+#define COMMON_CORE "1.2.22.12"
 #define COMMON_VERSION "Mani Admin Plugin 2012 V" COMMON_CORE
 
 #ifdef ORANGE
